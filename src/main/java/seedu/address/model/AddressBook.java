8ac8603a6964b8aa212ebf8904f4aa473baec8c3
fustilio <lfrancis373@gmail.com>
2017-10-04 15:25:34 +0800

    Trying set things straight

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -1,5 +1,14 @@
 package seedu.address.model;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
 import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
@@ -8,10 +17,6 @@ import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
-
-import java.util.*;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Wraps all data at the address-book level
@@ -48,13 +53,6 @@ public class AddressBook implements ReadOnlyAddressBook {
 
     public void setPersons(List<? extends ReadOnlyPerson> persons) throws DuplicatePersonException {
         this.persons.setPersons(persons);
-    }
-
-    public void removeTag(Tag tag) {
-        Iterator it = persons.iterator();
-        while(it.hasNext()) {
-            ((Person)it.next()).removeTag(tag);
-        }
     }
 
     public void setTags(Set<Tag> tags) {
