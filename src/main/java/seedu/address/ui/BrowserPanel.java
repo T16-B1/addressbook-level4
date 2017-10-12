09385b4d34a6339dc04dfa8693ccb55f2232f99b
visisapotato <vikviklim@msn.com>
2017-10-12 13:06:13 +0800

    changed browser to show google maps of postal codes

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -23,6 +23,7 @@ public class BrowserPanel extends UiPart<Region> {
     public static final String DEFAULT_PAGE = "default.html";
     public static final String GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com.sg/search?safe=off&q=";
     public static final String GOOGLE_SEARCH_URL_SUFFIX = "&cad=h";
+    public static final String GOOGLE_MAP_URL_PREFIX = "https://www.google.com.sg/maps/search/";
 
     private static final String FXML = "BrowserPanel.fxml";
 
@@ -44,6 +45,10 @@ public class BrowserPanel extends UiPart<Region> {
     private void loadPersonPage(ReadOnlyPerson person) {
         loadPage(GOOGLE_SEARCH_URL_PREFIX + person.getName().fullName.replaceAll(" ", "+")
                 + GOOGLE_SEARCH_URL_SUFFIX);
+    }
+
+    private void loadLocationPage(ReadOnlyPerson person) {
+        loadPage(GOOGLE_MAP_URL_PREFIX + person.getAddress().postalCode.value.substring(1));
     }
 
     public void loadPage(String url) {
@@ -68,6 +73,6 @@ public class BrowserPanel extends UiPart<Region> {
     @Subscribe
     private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        loadPersonPage(event.getNewSelection().person);
+        loadLocationPage(event.getNewSelection().person);
     }
 }
